c9a55186c659102caaa12874e19d68b54953173d
Samyak-Coder <73484421+Samyak-Coder@users.noreply.github.com>
2023-02-27 21:26:44 +0530

    Update client.py

--- client.py
+++ client.py
@@ -2,13 +2,15 @@ import socket
 from threading import Thread
 from tkinter import *
 from tkinter import ttk
+
 import ftplib
+from ftplib import FTP
+
 import os
-import ntpath #This is used to extract filename from path
-
+import ntpath 
 from tkinter import filedialog
-from pathlib import Path
 
+from pathlib import Path
 
 from playsound import playsound
 import pygame
@@ -44,6 +46,43 @@ def stop():
     mixer.music.load('shared_file/'+song_selected)
     mixer.music.pause()
     infoLabel.configure(text="")
+
+def resume():
+    global song_selected
+    mixer.int()
+    mixer.music.load('shared_file/'+song_selected)
+    mixer.music.play()
+
+def pause():
+    global song_selected
+    pygame
+    mixer.init()
+    mixer.music.load('shared_files/'+song_selected)
+    mixer.music.pause()
+
+def browseFile():
+    global Listbox
+    global song_counter
+    global filePathLabel
+
+    try:
+        filename = filedialog.askopenfilename()
+        HOSTNAME = "127.0.0.1"
+        USERNAME = "lftpd"
+        PASSWORD = "ldftpd"
+
+        ftp_server = FTP(HOSTNAME, USERNAME, PASSWORD)
+        ftp_server.encoding = "utf-8"
+        ftp_server.cwd('shared_files')
+        
+        fname = ntpath.basename(filename)
+        with open(filename, 'rb') as file:
+            ftp_server.storbinary(f"STOR {fname}", file)
+
+        ftp_server.dir()
+        ftp_server.quit()
+    except FileNotFoundError:
+        print("Cancle Button Pressed")
 
 def musicWindow():
     global song_counter
@@ -99,13 +138,20 @@ def musicWindow():
                   width=10,
                   bg="SkyBlue",
                   font=("Calibri", 10))
-    stop.place(x=30, y=250)
+    upload.place(x=30, y=250)
 
     infoLabel = Label(window, 
                       text="",
                       fg="blue",
                       font=("Calibri", 0))
     infoLabel.place(x=4, y=200)
+
+    ResumeBtn = Button(window, text="Resume", width=10, bd=1, bg="SkyBlue", font=("Calibri", 10), command=resume)
+    ResumeBtn.place(x=30, y=250)
+
+    PauseBtn = Button(window, text="Pause", width=10, bd=1, bg="SkyBlue", font=("Calibri", 10), command=pause)
+    PauseBtn.palce(x=200, y=250)
+
 
     window.mainloop()
 
