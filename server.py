e89e57e0571084d77c501d016220ec070712a72c
Samyak-Coder <73484421+Samyak-Coder@users.noreply.github.com>
2023-02-27 21:27:12 +0530

    Update server.py

--- server.py
+++ server.py
@@ -1,5 +1,11 @@
 import socket
-from threading import Thread
+from  threading import Thread
+import time
+import os
+
+from pyftpdlib.authorizers import DummyAuthorizer
+from pyftpdlib.handlers import FTPHandler
+from pyftpdlib.servers import FTPServer
 
 IP_ADDRESS = '127.0.0.1'
 PORT = 8050
@@ -7,41 +13,65 @@ SERVER = None
 BUFFER_SIZE = 4096
 clients = {}
 
-def acceptConections():
+
+is_dir_exists = os.path.isdir('shared_files')
+print(is_dir_exists)
+if(not is_dir_exists):
+    os.makedirs('shared_files')
+
+def ftp():
+    global IP_ADDRESS
+    authorizer = DummyAuthorizer()
+    authorizer.add_user("lftpd", "lftpd", ".", perm="elradfmw")
+
+    handler = FTPHandler
+    handler.authorizer = authorizer
+
+    ftp_server = FTPServer((IP_ADDRESS, 21), handler)
+    ftp_server.serve_forever()
+
+
+
+def acceptConnections():
     global SERVER
     global clients
 
     while True:
-        client, addr = SERVER.accept() 
-        clientName = client.recv(4096).decode().lower()
-        clients[clientName] = {
-            "client": client,
-            "address": addr, 
-            "connected_with": "",
-            "file_name": "",
-            "file_size": 4096
-        }
-        print(f"Connection established with {clientName}: {addr}")
+        client, addr = SERVER.accept()
+        client_name = client.recv(4096).decode().lower()
+        clients[client_name] = {
+                "client"         : client,
+                "address"        : addr,
+                "connected_with" : "",
+                "file_name"      : "",
+                "file_size"      : 4096
+            }
 
-        thread = Thread(target= handleClient, args={client, clientName})
-        thread.start()
-
+        print(f"Connection established with {client_name} : {addr}")
+        
 def setup():
-    print("\n\t\t\t\t\t\t IP MESSENGER\n")
+    print("\n\t\t\t\t\t\tIP MESSENGER\n")
 
     global PORT
     global IP_ADDRESS
     global SERVER
 
-    SERVER = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    SERVER  = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     SERVER.bind((IP_ADDRESS, PORT))
 
     SERVER.listen(100)
 
-    print("\t\t\t\t SERVER IS WAITING FOR INCOMING CONNECTIONS...")
+    print("\t\t\t\tSERVER IS WAITING FOR INCOMMING CONNECTIONS...")
     print("\n")
 
-    acceptConections()
+    acceptConnections()
 
-setup_thread = Thread(target=setup)
-setup_thread.start()    
+
+
+
+setup_thread = Thread(target=setup) 
+setup_thread.start()
+
+ftp_thread = Thread(target=ftp)
+ftp_thread.start()
